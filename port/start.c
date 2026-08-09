4ca1e98cf14df5d61813ccf33abd1e3ac4645503
sudhirvacharya <sudheerpaniyur@gmail.com>
2026-08-09 23:56:24 +0000

    update

--- port/start.c
+++ port/start.c
@@ -1,7 +1,24 @@
 #include "FreeRTOS.h"
 #include "task.h"
+
+static void vTask1(void *pvParameters) {
+    volatile int a = 0;
+    while(1){
+        a++;
+    };
+}
+static void vTask2(void *pvParameters) {
+    volatile int b = 0;
+    while(1){
+        b++;
+    };
+}
 void start()
 {
+    BaseType_t xReturn;
+    xReturn = xTaskCreate(vTask1, "T1", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    xReturn = xTaskCreate(vTask2, "T2", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+
     vTaskStartScheduler();
     return;
 }
